f3e93c38fb8d9aa6a5105e5e8dd267cdea430241
WonJin <try3982@kakao.com>
2025-10-16 19:39:44 +0900

    -. 자료구조: 배열

---
+++ DataStructure/Array/src/Practice.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+
+class MyArray {
+    int[] arr;
+
+    MyArray(int size) {
+        arr = new int[size];
+    }
+
+    public void insertData(int index, int data) {
+        if (index < 0 || index > arr.length) {
+            System.out.println("Index Error");
+            return;
+        }
+        int[] arrDup = this.arr.clone();
+        this.arr = new int[this.arr.length + 1];
+
+        for (int i = 0; i < index; i++) {
+            this.arr[i] = arrDup[i];
+        }
+
+        for (int i = index; i < this.arr.length; i++) {
+            this.arr[i] = arrDup[i - 1];
+        }
+
+        this.arr[index] = data;
+    }
+
+    public void removeData(int data) {
+        int targetIndex = -1;
+
+        for (int i = 0; i < this.arr.length; i++) {
+            if (this.arr[i] == data) {
+                targetIndex = i;
+                break;
+            }
+        }
+
+        if (targetIndex == -1) {
+            System.out.println("해당 데이터가 없습니다.");
+        } else {
+            int[] arrDup = this.arr.clone();
+            this.arr = new int[this.arr.length - 1];
+
+            for (int i = 0; i < targetIndex; i++) {
+                this.arr[i] = arrDup[i];
+            }
+
+            for (int i = targetIndex; i < this.arr.length; i++) {
+                this.arr[i] = arrDup[i + 1];
+            }
+        }
+    }
+}
+
+public class Practice {
+    public static void main(String[] args) {
+
+        int size = 5;
+        MyArray myArray = new MyArray(size);
+        for (int i = 0; i < size; i++) {
+            myArray.arr[i] = i+1;
+        }
+        System.out.println(Arrays.toString(myArray.arr));
+
+        myArray.arr[0] = 10;
+        System.out.println(Arrays.toString(myArray.arr));
+
+        myArray.insertData(2, 20);
+        System.out.println(Arrays.toString(myArray.arr));
+
+        myArray.removeData(2);
+        System.out.println(Arrays.toString(myArray.arr));
+
+    }
+}
