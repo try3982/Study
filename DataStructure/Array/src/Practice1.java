da912764a96afd5a5035e50cd2ed710531bd6b3d
WonJin <try3982@kakao.com>
2025-10-16 20:48:43 +0900

    -. 자료구조: 배열 연습문제 1

--- DataStructure/Array/src/Practice1.java
+++ DataStructure/Array/src/Practice1.java
@@ -1,7 +1,7 @@
 /**
  * 배열 arr의 모든 데이터에 대해서,
  * 짝수 데이터들의 평균과 홀수 데이터들의 평균을 출력하시오.
- *
+ * <p>
  * 입출력 예시_
  * 배열 arr : 1,2,3,4,5,6,7,8,9
  * 결과 :
@@ -12,24 +12,24 @@
 public class Practice1 {
     public static void main(String[] args) {
 
-        int arr[] = {1,2,3,4,5,6,7,8,9};
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         float sumEven = 0;
         float sumOdd = 0;
         int evenCnt = 0;
         int oddCnt = 0;
 
-        for(int item: arr) {
-            if(item % 2 == 0) {
-               sumEven += item;
+        for (int item : arr) {
+            if (item % 2 == 0) {
+                sumEven += item;
                 evenCnt++;
             } else {
-             sumOdd += item;
+                sumOdd += item;
                 oddCnt++;
             }
         }
 
-        System.out.println("짝수 평균 : " + sumEven/ evenCnt); // 5.0
-        System.out.println("홀수 평균 : " + sumOdd/ oddCnt);  // 5.0
+        System.out.println("짝수 평균 : " + sumEven / evenCnt); // 5.0
+        System.out.println("홀수 평균 : " + sumOdd / oddCnt);  // 5.0
 
     }
 }
