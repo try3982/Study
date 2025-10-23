88ab4d787df627f42046601e30fcd96733f665e4
WonJin <try3982@kakao.com>
2025-10-23 21:25:29 +0900

    -. 단어길이 재기

---
+++ coding-test/src/boj/problem/B5_2743_단어길이재기.java
@@ -0,0 +1,12 @@
+package boj.problem;
+
+import java.util.Scanner;
+
+public class B5_2743_단어길이재기 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String word = in.nextLine();
+
+        System.out.println(word.length());
+    }
+}
