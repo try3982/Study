3c251ca48009388224d46deb049109819501d1b5
WonJin <try3982@kakao.com>
2025-12-06 17:24:38 +0900

    -. [BOJ]역사를 걸으면 동국이 보이고

---
+++ coding-test/src/boj/problem/B5_34721.java
@@ -0,0 +1,15 @@
+package boj.problem;
+
+import java.util.Scanner;
+
+public class B5_34721 {
+    public static void main(String[] args) {
+
+        Scanner  in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for(int i=0; i<n; i++) {
+            System.out.println("I love DGU");
+        }
+    }
+}
